f3d9bcee9528f0faf03e7887f3c73d3ed79dade4
MahalakshmiPFW <maha.01@pfw.edu>
2024-10-26 14:01:56 -0400

    buyer header for buyer page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Logo from './components/common/Logo';
-import Footer from './components/common/Footer'; // Import Footer
+import Footer from './components/common/Footer';
 import Login from './pages/common/Login';
 import Signup from './pages/common/Signup';
 import ForgotPassword from './pages/common/ForgotPassword';
 import BuyerSellerChoice from './pages/common/BuyerSellerChoice';
 import BuyerDashboard from './pages/buyer/BuyerDashboard';
 import SellerDashboard from './pages/seller/SellerDashboard';
+import BuyerHeader from './components/buyer/BuyerHeader'; // Import BuyerHeader
 import './App.css';
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <div className="app d-flex flex-column min-vh-100">
+      <Logo />
+      {/* Conditionally render BuyerHeader only on the /buyer-dashboard route */}
+      {location.pathname === '/buyer-dashboard' && <BuyerHeader />}
+
+      {/* Main Content */}
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/choose-role" element={<BuyerSellerChoice />} />
+          <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+          <Route path="/seller-dashboard" element={<SellerDashboard />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="app d-flex flex-column min-vh-100"> {/* Full viewport height */}
-        <Logo />
-        <div className="flex-grow-1"> {/* Content area */}
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/choose-role" element={<BuyerSellerChoice />} />
-            <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
-            <Route path="/seller-dashboard" element={<SellerDashboard />} />
-            <Route path="/" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
